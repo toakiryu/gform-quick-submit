6a71efd751e4c23051f6319e09c228d8d3f72f92
Fun117 <toakiryu@gmail.com>
2024-12-24 13:34:08 +0900

    up

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://toakiryu.github.io",
+  url: "https://gform-quick-submit-docs.vercel.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/gform-quick-submit",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
